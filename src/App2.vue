6795df66c2e6f00d3bc48995ac7de85d121be2b7
RKP <ajarubab@gmail.com>
2024-06-17 20:17:50 +0530

    replicating existing App vue component as App2 vue component

---
+++ src/App2.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>This is H1 tag in App2.vue</h1>
+<HelloGuyz name="Hello guys how are you"/>
+</template>
+
+  
+  
+<script>
+import HelloGuyz from './components/HelloGuyz.vue'
+
+export default {
+    name: 'App_two',
+    components: {
+        HelloGuyz
+    }
+}
+</script>
+  
+  
+<style>
+  </style>
---
+++ src/components/HelloGuyz.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="guyz">
+      <h1>{{ name }}</h1>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'HelloGuyz',
+  props: {
+    name: String
+  }
+}
+</script>
